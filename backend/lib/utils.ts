4934e83bf7f99d19a056dd32a4ae13ee0259f543
klimjs <klimsemenov86@gmail.com>
2026-01-17 19:51:52 +0100

    chore: prettier

--- backend/lib/utils.ts
+++ backend/lib/utils.ts
@@ -1,10 +1,13 @@
-export function buildResponse(statusCode: number, body: Record<string, unknown>) {
+export function buildResponse(
+  statusCode: number,
+  body: Record<string, unknown>,
+) {
   return {
     statusCode,
     headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/json",
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(body),
-  };
+  }
 }
