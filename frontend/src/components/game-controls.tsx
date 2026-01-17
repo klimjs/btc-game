ebaddc00e5d19f3c46d0530e1d0de3bfc6f49adf
klimjs <klimsemenov86@gmail.com>
2026-01-17 13:53:07 +0100

    feat: game controls info ui

--- frontend/src/components/game-controls.tsx
+++ frontend/src/components/game-controls.tsx
@@ -1,15 +1,34 @@
 import { ButtonGroup } from '@/components/ui/button-group'
 import { Button } from '@/components/ui/button'
-import { TrendingDown, TrendingUp } from 'lucide-react'
+import { Dice2, TrendingDown, TrendingUp } from 'lucide-react'
+import {
+  Item,
+  ItemContent,
+  ItemDescription,
+  ItemMedia,
+  ItemTitle,
+} from './ui/item'
 
 export const GameControls = () => {
   return (
-    <div className="flex justify-center py-10">
+    <div className="flex flex-col items-center justify-center py-10 gap-10">
+      <Item variant="muted">
+        <ItemMedia variant="icon">
+          <Dice2 />
+        </ItemMedia>
+        <ItemContent>
+          <ItemTitle>Try to guess!</ItemTitle>
+        </ItemContent>
+        <ItemContent>
+          <ItemDescription>0:60</ItemDescription>
+        </ItemContent>
+      </Item>
+
       <ButtonGroup aria-label="Game controls">
-        <Button variant="outline" size="icon-lg">
+        <Button variant="outline" className="size-12">
           <TrendingDown />
         </Button>
-        <Button variant="outline" size="icon-lg">
+        <Button variant="outline" className="size-12">
           <TrendingUp />
         </Button>
       </ButtonGroup>
