25c75afecbc74076b09b1e3462e7d2b82a0ae81b
klimjs <klimsemenov86@gmail.com>
2026-01-17 13:16:14 +0100

    fix: score formatting

--- frontend/src/components/score.tsx
+++ frontend/src/components/score.tsx
@@ -4,10 +4,7 @@ export const Score = () => {
   return (
     <>
       <div className="flex gap-2 font-medium text-2xl">
-        Your score:{' '}
-        <Badge className="text-2xl h-8" variant="secondary">
-          0
-        </Badge>
+        Your score: <Badge className="text-2xl h-8">0</Badge>
       </div>
       <div className="text-muted-foreground">
         Try to guess whether the market price of Bitcoin (BTC/USD) will be
